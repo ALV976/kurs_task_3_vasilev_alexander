4bdc0886f51c6807e4916be4fddb616adb2116cc
alex <modernswk@gmail.com>
2024-01-05 00:03:54 +0300

    begin

---
+++ functions.py
@@ -0,0 +1,17 @@
+def output_last_tran(date_trans, descr_trans, ):
+    """Основная распечатка"""
+
+    print(f"<дата перевода> {date_trans} <описание перевода> {descr_trans}")
+    print(f'<откуда> -> <куда>')
+    print(f'<сумма перевода> <валюта>')
+
+def get_format_date():
+    pass
+
+
+def get_mask_card_number():
+    pass
+
+
+def get_mask_account_number():
+    pass
