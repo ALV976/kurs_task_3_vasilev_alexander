af588bde862b3c30aced947325dd1b4eea38c2a4
alex <modernswk@gmail.com>
2024-01-05 00:07:40 +0300

    begin

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
-def output_last_tran(date_trans, descr_trans, ):
+def output_last_tran(date_trans, descr_trans, trans_from):
     """Основная распечатка"""
 
     print(f"<дата перевода> {date_trans} <описание перевода> {descr_trans}")
-    print(f'<откуда> -> <куда>')
+    print(f'<откуда>{trans_from} -> <куда>')
     print(f'<сумма перевода> <валюта>')
+
 
 def get_format_date():
     pass
